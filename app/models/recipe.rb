ae59b5f2a04a78ae3e2e488548f017edd4bfe4a3
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-08-12 22:47:42 +0100

    Recipe form styled

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,7 @@ class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   has_many :directions, dependent: :destroy
   validates :title, presence: true
+  validates :image, presence: true
+
   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
