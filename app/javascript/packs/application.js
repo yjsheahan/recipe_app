f61ecd39561ab6a659a134d816c3e0d99f5ef46e
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-08-12 20:23:02 +0100

    log in, edit and sign up pages styled

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -26,7 +27,6 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-import { formtabs } from '../plugins/formtabs';
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
